c61749ca7f7aa55c4b0c24990391019c59e2d566
Rahad-Ullah <Rahad-Ullah@users.noreply.github.com>
2023-09-02 12:09:45 +0600

    Display Dynamic Data on cards

--- js/main.js
+++ js/main.js
@@ -1,31 +1,37 @@
-const loadVideos = async () =>{
-    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/1000`)
+// Load videos if any category button is clicked
+const loadVideos = async (categoryId) =>{
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/category/${categoryId}`)
     const data = await res.json();
     const videos = data.data;
     displayVideo(videos);
 }
 
+// Display videos of the specific category
 const displayVideo = (videos) => {
     const videoContainer = document.getElementById('video-container');
+    videoContainer.textContent = ''
+    
+    handleSearchEmpty(videos);
 
     videos.forEach(video => {
+        console.log(video)
         const videoCard = document.createElement('div');
         videoCard.classList.add('space-y-5');
         videoCard.innerHTML = `
-        <figure>
-            <img src="images/d72ed80d0c125cb4d7fdda46f2bb42c0.jpeg" alt="image" class="rounded-lg">
+        <figure class="">
+            <img src="${video?.thumbnail}" alt="image" class="rounded-lg h-48 w-full">
         </figure>
         <div class="flex gap-3">
             <figure class="">
-                <img src="images/user1.jpeg" alt="user-photo" class="w-16 rounded-full">
+                <img src="${video?.authors[0]?.profile_picture}" alt="user-photo" class="w-8 h-8 rounded-full">
             </figure>
             <div class="space-y-2">
-                <h3 class="font-bold text-zinc-950">Building a Winning UX Strategy Using the Kano Model</h3>
+                <h3 class="font-bold text-zinc-950">${video?.title}</h3>
                 <div class="flex gap-2">
-                    <p class="text-sm text-zinc-700">Awlad Hossain</p>
-                    <img src="images/tickmark.png" alt="">
+                    <p class="text-sm text-zinc-700">${video?.authors[0]?.profile_name}</p>
+                    <img src="${video?.authors[0]?.verified ? 'images/tickmark.png' : ''}" alt="">
                 </div>
-                <p class="text-sm text-zinc-700">91K views</p>
+                <p class="text-sm text-zinc-700">${video?.others?.views} views</p>
             </div>
         </div>
         `
@@ -33,6 +39,26 @@ const displayVideo = (videos) => {
     });
 }
 
-loadVideos()
+
+// Load categories buttons
+const loadCategories = async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/videos/categories`);
+    const data = await res.json();
+    const categories = data.data;
+    handleCategories(categories)
+}
+loadCategories();
+
+// Activate categories buttons
+const handleCategories = (categories) =>{
+    categories.forEach(category =>{
+        const categoryId = category.category_id;
+        document.getElementById(categoryId).addEventListener('click', () => {
+            loadVideos(categoryId)
+        })
+    })
+}
 
 
+
+loadVideos(1000)
